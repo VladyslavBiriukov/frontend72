45ca2e219110fdb2b75cec58e4010ab5a001e983
Vladyslav Biriukov <vladislavbiriukovsquirrel@gmail.com>
2022-12-09 23:08:02 +0200

    scss done

---
+++ js/switch.js
@@ -0,0 +1,11 @@
+// let switchMode = document.getElementById("switchMode");
+
+// switchMode.onclick = function () {
+//     let theme = document.getElementById("theme");
+
+//     if (theme.getAttribute("href") == "styles.css") {
+//         theme.href = "light-mode.css";
+//     } else {
+//         theme.href = "styles.css";
+//     }
+// }
